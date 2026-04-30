1c370f991fda6b844b923a35907518502d1b27cf
SUGIRA Gabriel <sugiragbm@gmail.com>
2026-04-30 20:17:32 +0200

    time commit back3

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI || 'mongodb+srv://sugiragbm:muyobokeSugira@cluster0.f36egnm.mongodb.net/?appName=Cluster0';
+    const uri = 'mongodb+srv://sugiragbm:muyobokeSugira@cluster0.f36egnm.mongodb.net/?appName=Cluster0/timetable';
     const conn = await mongoose.connect(uri);
     console.log(`MongoDB Atlas Connected: ${conn.connection.host}`);
   } catch (error) {
--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://time-frontend.onrender.com', 'http://localhost:3006', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
