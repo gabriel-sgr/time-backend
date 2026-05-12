782a30470e42a105075d0be1a13cbbd48b64eb75
SUGIRA Gabriel <sugiragbm@gmail.com>
2026-05-12 19:27:39 +0200

    new timever23

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
